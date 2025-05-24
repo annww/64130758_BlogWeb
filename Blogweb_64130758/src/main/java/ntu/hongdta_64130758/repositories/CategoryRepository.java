b18095a5a4d47998e31a8bc56d621f27eddc601e
Anh Hong <samh5813p@gmail.com>
2025-05-24 10:20:42 +0700

    delete dự án thừa

--- Blogweb_64130758/src/main/java/ntu/hongdta_64130758/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package ntu.hongdta_64130758.repositories;
-
-import java.util.Locale.Category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {}
--- Blogweb_64130758/src/main/java/ntu/hongdta_64130758/repositories/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package ntu.hongdta_64130758.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ntu.hongdta_64130758.models.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long> {}
